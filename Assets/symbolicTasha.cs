186d42982960cf6f046cb607dcb2d42981fadf09
tasthing <lufarionite@gmail.com>
2020-01-28 21:54:34 -0500

    Initial commit?

---
+++ Assets/symbolicTasha.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+using rnd = UnityEngine.Random;
+
+public class symbolicTasha : MonoBehaviour
+{
+    public new KMAudio audio;
+    public KMBombInfo bomb;
+	public KMBombModule module;
+
+	public KMSelectable[] buttons;
+	public Renderer[] btnRenderers;
+	public Renderer[] buttonSymbols;
+	public Light[] lights;
+	public Color[] materialColors;
+	public Texture[] symbols;
+	public Texture crackedTexture;
+
+	private int[] flashing = new int[5];
+	private int[] presentSymbols = new int[4];
+	private List<int> solution = new List<int>();
+	private int stage;
+	private int enteringStage;
+	private int[] buttonColors = new int[4] { 0, 1, 2, 3 };
+	private bool[] trueConditions = new bool[4] { false, false, false, true };
+	private int currentTable;
+	private bool[] cracked = new bool[4];
+
+	private string[] soundNames = new string[4] { "High", "NotAsHigh", "NotAsHighAsNotAsHigh", "NotHigh" };
+	private static readonly string[] positionNames = new string[4] { "top", "right", "bottom", "left" };
+	private static readonly string[] colorNames = new string[4] { "pink", "green", "yellow", "blue" };
+	private static readonly string[] vanillaIndicators = new string[11] { "SNR", "CLR", "CAR", "IND", "FRQ", "SIG", "NSA", "MSA", "TRN", "BOB", "FRK" };
+	private bool anyBtnPressed;
+	private bool[] flashingButtons = new bool[4];
+
+    private static int moduleIdCounter = 1;
+    private int moduleId;
+    private bool moduleSolved;
+
+    void Awake()
+    {
+    	moduleId = moduleIdCounter++;
+		foreach (KMSelectable button in buttons)
+			button.OnInteract += delegate () { ButtonPress(button); return false; };
+    }
+
+    void Start()
+    {
+		if (bomb.GetPortPlates().Any(x => x.Length == 0))
+			trueConditions[2] = true;
+		float scalar = transform.lossyScale.x;
+        for (int i = 0; i < 4; i++)
+        {
+        	lights[i].range *= scalar;
+        	lights[i].enabled = false;
+        }
+		buttonColors = buttonColors.Shuffle().ToArray();
+        soundNames = soundNames.Shuffle().ToArray();
+		for (int i = 0; i < 4; i++)
+        {
+            Debug.LogFormat("[Symbolic Tasha #{0}] The {1} button is {2}.", moduleId, positionNames[i], colorNames[buttonColors[i]]);
+            btnRenderers[i].material.color = materialColors[buttonColors[i]];
+            lights[i].color = materialColors[buttonColors[i]];
+			presentSymbols[i] = rnd.Range(1,19);
+			buttonSymbols[i].material.mainTexture = symbols[presentSymbols[i] - 1];
+        }
+		string[] ordinals = new string[5] { "first", "second", "third", "fourth", "fifth" };
+		for (int i = 0; i < 5; i++)
+		{
+			flashing[i] = rnd.Range(0,4);
+			Debug.LogFormat("[Symbolic Tasha #{0}] The {1} flashing color in the sequence is {2}.", moduleId, ordinals[i], colorNames[flashing[i]]);
+		}
+		solution.Add(CalculateStage());
+		//Debug.LogFormat("[Symbolic Tasha #{0}] ix = {1}.", moduleId, Array.IndexOf(Tables.symbolColumns[currentTable], Tables.symbolColumns[currentTable].Where(a => a.Contains(presentSymbols[Array.IndexOf(buttonColors, flashing[stage])])).First()));
+		Debug.LogFormat("[Symbolic Tasha #{0}] The first color to press is {1}.", moduleId, colorNames[solution[0]]);
+		StartCoroutine(SequenceFlash());
+    }
+
+	void ButtonPress(KMSelectable button)
+	{
+		anyBtnPressed = true;
+		var ix = Array.IndexOf(buttons, button);
+		button.AddInteractionPunch(2);
+		audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, button.transform);
+		audio.PlaySoundAtTransform(soundNames[ix], button.transform);
+		if (!cracked[ix])
+		{
+			cracked[ix] = true;
+			presentSymbols[ix] *= -1;
+			btnRenderers[ix].material.mainTexture = crackedTexture;
+			buttonSymbols[ix].gameObject.SetActive(false);
+			audio.PlaySoundAtTransform("shatter", button.transform);
+			if (!moduleSolved)
+				Debug.LogFormat("[Symbolic Tasha #{0}] The {1} button cracked!", moduleId, colorNames[buttonColors[ix]]);
+		}
+		if (moduleSolved)
+			return;
+		if (enteringStage == 0)
+		{
+			StopAllCoroutines();
+			foreach (Light l in lights)
+				l.enabled = false;
+		}
+		StartCoroutine(SingleFlash(ix));
+		if (buttonColors[ix] != solution[enteringStage])
+		{
+			Debug.LogFormat("[Symbolic Tasha #{0}] You pressed the {1} button. That was incorrect. Strike!", moduleId, colorNames[buttonColors[ix]]);
+			StartCoroutine(Strike());
+		}
+		else
+			enteringStage++;
+		if (enteringStage > stage)
+			StartCoroutine(AdvanceStage());
+	}
+
+	IEnumerator AdvanceStage()
+	{
+		enteringStage = 0;
+		stage++;
+		if (stage == 5)
+		{
+			module.HandlePass();
+			moduleSolved = true;
+			Debug.LogFormat("[Symbolic Tasha #{0}] Module solved!", moduleId);
+		}
+		else
+		{
+			while (flashingButtons.Contains(true))
+				yield return null;
+			solution.Add(CalculateStage());
+			StartCoroutine(SequenceFlash());
+			Debug.LogFormat("[Symbolic Tasha #{0}] {1} was added to the sequence of colors to press.", moduleId, colorNames[solution[stage]]);
+		}
+	}
+
+	IEnumerator Strike()
+	{
+		module.HandleStrike();
+		enteringStage = 0;
+		while (flashingButtons.Contains(true))
+			yield return null;
+		StartCoroutine(SequenceFlash());
+	}
+
+	IEnumerator SingleFlash(int ix)
+	{
+		flashingButtons[ix] = true;
+		lights[ix].enabled = true;
+		yield return new WaitForSeconds(.5f);
+		lights[ix].enabled = false;
+		flashingButtons[ix] = false;
+	}
+
+	IEnumerator SequenceFlash()
+	{
+		while (flashingButtons.Contains(true))
+			yield return null;
+		yield return new WaitForSeconds(1.75f);
+		sequenceReset:
+		for (int i = 0; i <= stage; i++)
+		{
+			var ix = Array.IndexOf(buttonColors, flashing[i]);
+			lights[ix].enabled = true;
+			if (anyBtnPressed)
+				audio.PlaySoundAtTransform(soundNames[ix], buttons[ix].transform);
+			yield return new WaitForSeconds(.8f);
+			lights[ix].enabled = false;
+			yield return new WaitForSeconds(.4f);
+		}
+		yield return new WaitForSeconds(3.5f);
+		goto sequenceReset;
+	}
+
+	int CalculateStage()
+	{
+		if (Tables.symbolColumns[currentTable].Count(a => a.Contains(presentSymbols[Array.IndexOf(buttonColors, flashing[stage])])) == 0)
+			return 0;
+		else
+		{
+			var ix = Array.IndexOf(Tables.symbolColumns[currentTable], Tables.symbolColumns[currentTable].Where(a => a.Contains(presentSymbols[Array.IndexOf(buttonColors, flashing[stage])])).First());
+			return Tables.colorRows[currentTable][flashing[stage]][ix];
+		}
+	}
+
+	void Update()
+	{
+		if (!moduleSolved)
+		{
+			if (bomb.GetStrikes() >= 2 && !trueConditions[0])
+			{
+				trueConditions[0] = true;
+				Debug.LogFormat("[Symbolic Tasha #{0}] The number of strikes has reached 2 or more. The first condition is now true.", moduleId);
+			}
+			if (cracked.Count(b => b) >= 2 && !trueConditions[1])
+			{
+				trueConditions[1] = true;
+				Debug.LogFormat("[Symbolic Tasha #{0}] The number of cracked buttons has reached 2 or more. The second condition is now true.", moduleId);
+			}
+			currentTable = Array.IndexOf(trueConditions, true);
+		}
+	}
+}
---
+++ Assets/tables.cs
@@ -0,0 +1,121 @@
+public static class Tables
+{
+	// 1 = Hook
+	// 2 = Wand
+	// 3 = Yankee
+	// 4 = Lightning
+	// 5 = India
+	// 6 = Angle
+	// 7 = Kilo
+	// 8 = Flag w/ Hole
+	// 9 = Foxtrot
+	// 10 = Antenna
+	// 11 = Compass
+	// 12 = Half-Fire
+	// 13 = Left Triangle
+	// 14 = Circle
+	// 15 = Shelf
+	// 16 = Curve
+	// 17 = Lollipop
+	// 18 = Flag w/o Hole
+	// -x = Cracked variant of x
+	public static readonly int[][][] symbolColumns = new int[4][][]
+	{
+		new int[12][]
+		{
+			new int[] { 1, 9 },
+			new int[] { 2, -3 },
+			new int[] { -2, -5 },
+			new int[] { 3, -10 },
+			new int[] { -4, -11 },
+			new int[] { 5, 10 },
+			new int[] { -6, -12 },
+			new int[] { 7, 13 },
+			new int[] { -1, -9 },
+			new int[] { -7, -13 },
+			new int[] { -8, 6 },
+			new int[] { 4, 11 }
+		},
+		new int[12][]
+		{
+			new int[] { -12, 9 },
+			new int[] { 2, -3 },
+			new int[] { -11, -5 },
+			new int[] { 3, 1 },
+			new int[] { -4, -14 },
+			new int[] { 6, 4 },
+			new int[] { 14, 15 },
+			new int[] { -13, -9 },
+			new int[] { -1, 13 },
+			new int[] { 12, 7 },
+			new int[] { -8, 16 },
+			new int[] { 8, 11 }
+		},
+		new int[12][]
+		{
+			new int[] { 17, 6 },
+			new int[] { -1, -5 },
+			new int[] { 9, -18 },
+			new int[] { 18, 8 },
+			new int[] { 4, 2 },
+			new int[] { -6, 14 },
+			new int[] { 11, -3 },
+			new int[] { -16, 5 },
+			new int[] { -17, -2 },
+			new int[] { -15, 10 },
+			new int[] { 1, -14 },
+			new int[] { 16, -8 }
+		},
+		new int[12][]
+		{
+			new int[] { 11, -5 },
+			new int[] { 5, -12 },
+			new int[] { -2, -16 },
+			new int[] { 16, 14 },
+			new int[] { -4, 15 },
+			new int[] { -10, 3 },
+			new int[] { -6, -17 },
+			new int[] { 10, 17 },
+			new int[] { -1, -3 },
+			new int[] { -7, -15 },
+			new int[] { 12, 6 },
+			new int[] { -13, 11 }
+		},
+	};
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// 0 = Pink
+	// 1 = Green
+	// 2 = Yellow
+	// 3 = Blue
+	public static readonly int[][][] colorRows = new int[4][][]
+	{
+		new int[4][]
+		{
+			new int[12] { 1, 0, 1, 1, 0, 3, 3, 1, 0, 0, 2, 1 },
+			new int[12] { 0, 1, 3, 2, 1, 1, 0, 3, 1, 3, 1, 3 },
+			new int[12] { 2, 3, 2, 0, 2, 2, 2, 0, 2, 1, 0, 2 },
+			new int[12] { 3, 2, 0, 3, 3, 0, 1, 2, 3, 2, 3, 0 }
+		},
+		new int[4][]
+		{
+			new int[12] { 1, 0, 2, 2, 0, 3, 0, 3, 1, 0, 1, 1 },
+			new int[12] { 2, 3, 0, 1, 1, 0, 2, 1, 0, 3, 3, 2 },
+			new int[12] { 3, 2, 3, 0, 2, 2, 1, 0, 3, 1, 0, 3 },
+			new int[12] { 0, 1, 1, 3, 3, 1, 3, 2, 2, 2, 2, 0 }
+		},
+		new int[4][]
+		{
+			new int[12] { 3, 3, 1, 3, 0, 1, 2, 2, 1, 2, 0, 3 },
+			new int[12] { 1, 0, 0, 1, 2, 0, 3, 0, 2, 0, 1, 1 },
+			new int[12] { 0, 2, 3, 0, 3, 3, 1, 1, 3, 1, 2, 0 },
+			new int[12] { 2, 1, 2, 2, 1, 2, 0, 3, 0, 3, 3, 2 }
+		},
+		new int[4][]
+		{
+			new int[12] { 2, 0, 3, 3, 3, 1, 0, 0, 3, 2, 1, 0 },
+			new int[12] { 1, 2, 2, 0, 1, 0, 1, 3, 1, 0, 2, 3 },
+			new int[12] { 3, 3, 0, 1, 0, 3, 2, 2, 2, 3, 0, 1 },
+			new int[12] { 0, 1, 1, 2, 2, 2, 3, 1, 0, 1, 3, 2 }
+		}
+	};
+}
